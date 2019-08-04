13caa4d983b5c7f8ef784a95a0162932481aa94e
hudaokeji <github@hudaokeji.com>
2019-08-04 11:52:16 +0900

    init commit

---
+++ dist/skylark-jquery-form/main.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-jquery-form - A version of jquery-form.js that ported to running on skylarkjs.
+ * @author Hudaokeji, Inc.
+ * @version v0.9.0
+ * @link https://github.com/skylark-integration/skylark-jquery-form/
+ * @license MIT
+ */
+define(["./form"],function(n){return n});
+//# sourceMappingURL=sourcemaps/main.js.map
---
+++ dist/uncompressed/skylark-jquery-form/main.js
@@ -0,0 +1,5 @@
+define([
+	"./form"
+],function(form){
+	return form;
+});
